4165268fdd86fcc831794c4c3d73f049d0f97a74
ZanZakZmak <58043160+ZanZakZmak@users.noreply.github.com>
2022-10-11 08:34:59 +0200

    rješen zadatak3

---
+++ Blic01/blicZadatak3.js
@@ -0,0 +1,10 @@
+//sortiraj mjesani arrey
+function sortiranje(arr) {
+  var sortirani_arr = [];
+  sortirani_arr = arr.sort();
+  return sortirani_arr;
+}
+
+console.log(
+  sortiranje([321, 63, "Marmelada", 4, "Kruh", 1, 1234, "Tanjur", 50])
+);
